df7d3b5abcb2675a384cfaccd2ccf186e5453c0b
Levi <levicarlson123@gmail.com>
2020-06-28 19:54:39 -0500

    inputs added

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Button, Space } from 'antd';
+import { Form, Input, Button, Space } from 'antd';
 import { StopOutlined, SaveOutlined } from '@ant-design/icons';
 import { withRouter } from 'react-router-dom';
 
-class Edit extends Component {
-  state = {
-    edit: {
-      title: '',
-      description: ''
-    }
-  }
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 
+const validateMessages = {
+  required: '${label} is required!',
+};
+
+class Edit extends Component {
   render() {
-    const { movies, genres, details, history } = this.props;
+    const { movies, details, history } = this.props;
+    const onFinish = values => {
+      console.log(values);
+      this.props.dispatch({ type: ''})
+    };
     return (
       <>
+        <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+          <Form.Item name={['movie', 'title']} label="Title" rules={[{ required: true }]}>
+            <Input onChange={event => this.setState({
+              edit: {
+                title: event.target.value
+              }
+            })} />
+          </Form.Item>
+          <Form.Item name={['movie', 'description']} label="Description" rules={[{ required: true }]}>
+            <Input.TextArea onChange={event => this.setState({
+              edit: {
+                description: event.target.value
+              }
+            })} />
+          </Form.Item>
+          <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
         <Space>
           <Button
             type='default'
